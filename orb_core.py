3369c7fba2b285f18e6f734c9fe6e4a922b35601
AriBowe <44865080+AriBowe@users.noreply.github.com>
2019-05-10 19:07:20 +1000

    Major update
    VSCode died so manual upload
    
    Added new reactions, a ton of commands, and some other stuff

--- orb_core.py
+++ orb_core.py
@@ -55,17 +55,69 @@ async def on_message(message):
                 output += "Arguments: " + command[2] + "```"
             await message.channel.send(output)
 
+        # Secreto
         elif message.content.startswith(PREFIX + "secret"):
             print("Secret called. " + message.author.display_name + " is curious")
             await message.channel.send(random.choice([":wink:", "Shhhh", ":thinking:"]))
-        
-        #Fallback unrecognised commands
+
+        # Meme ban
+        elif message.content.startswith(PREFIX + "ban"):
+            target = message.content.split(PREFIX + "ban")[1]
+            print("Ban on", target, "called for by", message.author.display_name)
+            if "ORB" in target.upper() or "<@569758271930368010>" in target:
+                await message.channel.send("Pls no ban am good bot")
+            elif message.content == (PREFIX + "ban") or message.content == (PREFIX + "ban "):
+                await message.channel.send(random.choice(["I can't ban nothing", "Specifiy a person please", "This command doesn't work like that"]))
+            else:
+                await message.channel.send("Banning " + target)
+                await message.channel.send("...")
+                await message.channel.send("Shit it didn't work")
+
+        # Bully time
+        elif message.content.startswith(PREFIX + "bully") or message.content.startswith(PREFIX + "bulli"):
+            print("Bully called for by", message.author.display_name)
+            try:
+                target = message.content.split(PREFIX + "bully")[1]
+            except:
+                target = message.content.split(PREFIX + "bulli")[1]
+            if "ORB" in target.upper() or "<@569758271930368010>" in target:
+                await message.channel.send("You cannot kill me :superiorsmuggie:")
+            elif message.content == PREFIX + "bully" or message.content == PREFIX + "bully " or message.content == PREFIX + "bulli" or message.content == PREFIX + "bulli ":
+                await message.channel.send(random.choice(["I can't kill nothing", "Sic me on them, boss", "That's not how this works", "Who?"]))
+            else:
+                await message.channel.send(random.choice([("Bullying " + target), (target + " is a meanie!"), (target + " please stop speaking")]))
+
+        # Ranking
+        elif message.content.startswith(PREFIX + "rank"):
+            target = message.content.split(PREFIX + "rank")[1]
+            print("Ranking", target, "for", message.author.display_name)
+            if "ORB" in target.upper() or "<@569758271930368010>" in target:
+                await message.channel.send("I'd give me a 10/10")
+            else:
+                await message.channel.send("I'd give " + target + " a " + str(random.randint(0, 10)) + " out of 10")
+
+        # Illya
+        elif message.content.startswith(PREFIX + "illya"):
+            print("Illya called by", message.author.display_name)
+            if random.randint(1, 50) == 2:
+                await message.channel.trigger_typing()
+                await message.channel.send(file=discord.File(fp="images/lolice.gif"))
+            else:
+                await message.channel.trigger_typing()
+                await message.channel.send(file=discord.File(fp="images/illya_" + str(random.randint(1, 11)) + ".png"))
+
+        # BDE
+        elif message.content.startswith(PREFIX + "bde"):
+            print("BDE called by", message.author.display_name)
+            await message.channel.send("Isn't that Todd's job?")
+
+        # Fallback unrecognised commands
         else:
             print("Unrecognised input detected:", message.content)
-            await message.channel.send("Error: Command not recognised. Try " + PREFIX + "help")
+            await message.channel.send("Error: Command not recognised. Try " + PREFIX + "help. Remember, all commands are lowercase!")
 
     # If message contains very cool, or otherwise a 1/2000 chance of reacting "very cool"
-    if "VERY COOL" in message.content.upper() or random.randrange(1, 2000) == 1:
+    if "VERY COOL" in message.content.upper() or random.randint(1, 2000) == 1:
         await message.add_reaction("🇻")
         await message.add_reaction("🇪")
         await message.add_reaction("🇷")
@@ -78,7 +130,7 @@ async def on_message(message):
     
     # Girls aren't real
     elif "GIRLS AREN'T REAL" in message.content.upper() or "GIRLS ARENT REAL" in message.content.upper():
-        rand_int = random.randrange(1, 10)
+        rand_int = random.randint(1, 10)
         if rand_int <= 3:
             await message.add_reaction("🇹")
             await message.add_reaction("🇷")
@@ -97,7 +149,7 @@ async def on_message(message):
 
     # Epic reaction time
     elif "EPIC" in message.content.upper():
-        if random.randrange(1, 20) == 1:
+        if random.randint(1, 20) == 2:
             await message.add_reaction("🇪")
             await message.add_reaction("🅱")
             await message.add_reaction("🇮")
@@ -109,6 +161,10 @@ async def on_message(message):
             await message.add_reaction("🇮")
             await message.add_reaction("🇨")
             print("Reacted 'epic' to the message", "'" + message.content + "'", "from user", message.author.display_name)
-            
+    
+    # Memory meme
+    elif random.randint(1, 2000) == 1:
+        print("I remember", message.content)
+        await message.channel.send("[Orb will remember that]")
 
-client.run("nope")
+client.run("No steal >:)")
